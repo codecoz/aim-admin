0da7f8466dd9d38c266cf0afaa767710379bfc97
takielias <taki.ahmad@brainstation-23.com>
2024-05-23 11:41:34 +0600

    Update Menu

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -2,7 +2,12 @@
 
 @inject('sidebarItemHelper', 'CodeCoz\AimAdmin\MenuBuilder\SidebarItemHelper')
 
-@if ($sidebarItemHelper->isSubmenu($item))
+@if ($sidebarItemHelper->isHeader($item))
+
+    {{-- Header --}}
+    <x-aim-admin::sidebar.header :item="$item"/>
+
+@elseif ($sidebarItemHelper->isSubmenu($item))
 
     {{-- Treeview menu --}}
     <x-aim-admin::sidebar.treeview :item="$item"/>
--- resources/views/components/sidebar/link.blade.php
+++ resources/views/components/sidebar/link.blade.php
@@ -1,13 +1,12 @@
 @props(['item'])
 <li @isset($item['id']) id="{{ $item['id'] }}" @endisset class="nav-item">
-    <a class="nav-link {{ $item['class'] }} @isset($item['shift']) {{ $item['shift'] }} @endisset"
-       href="{{ $item['href'] }}" @isset($item['target']) target="{{ $item['target'] }}" @endisset
+    <a class="nav-link {{ $item['class']??'' }} {{$item['shift']??''}}"
+       href="{{ $item['href']??'' }}" @isset($item['target']) target="{{ $item['target'] }}" @endisset
         {!! $item['data-compiled'] ?? '' !!}>
 
         <i class="{{ $item['icon'] ?? 'far fa-fw fa-circle' }} {{
             isset($item['icon_color']) ? 'text-'.$item['icon_color'] : ''
         }}"></i>
-
         <p>
             {{ $item['text'] }}
 
@@ -17,6 +16,5 @@
                 </span>
             @endisset
         </p>
-
     </a>
 </li>
